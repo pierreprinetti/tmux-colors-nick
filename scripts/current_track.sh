91f58915951cf9d77faae38d5fb4af52dd7ef1bf
Pierre Prinetti <me@qrawl.net>
2017-02-21 12:11:19 +0100

    use TPM plugins path environment var

--- scripts/current_track.sh
+++ scripts/current_track.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-command -v osascript >/dev/null 2>&1 && { osascript scripts/current_track.scpt }
+command -v osascript >/dev/null 2>&1 && echo -n $(osascript ${TMUX_PLUGIN_MANAGER_PATH}tmux-colors-nick/scripts/current_track.scpt)
